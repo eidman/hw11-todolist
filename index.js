064155b34fa6ab5473d646595465972fb2d8256f
eidman <eidmanx@gmail.com>
2020-01-22 10:02:44 -0500

    css tweaks

--- index.js
+++ index.js
@@ -5,32 +5,71 @@
 // ● Bonus 2 - Begin styling your todo list
 
 document.addEventListener("DOMContentLoaded", function() {
-  var eat = ["Eat", "Sleep", "Die"];
+  var eatSleepDie = ["EAT", "SLEEP", "DIE"];
 
-  //    forEach loop
-  eat.forEach(function(eat, index) {
-    var li = document.createElement("li");
-    li.innerHTML = eat;
-    var container = document.querySelector(".list");
+  // forEach loop
+  eatSleepDie.forEach(function(eatSleepDie, index) {
+    let li = document.createElement("li");
+    li.innerHTML = eatSleepDie;
+    let container = document.querySelector(".list");
     container.appendChild(li);
   });
 
-  var form = document.querySelector("form");
-  form.addEventListener("submit", function(event) {
+  loadEvents();
+  // load every event in the page
+  function loadEvents() {
+    document.querySelector("form").addEventListener("submit", submit);
+    document.getElementById("clear").addEventListener("click", clearList);
+    document.querySelector("ul").addEventListener("click", deleteOrTick);
+  }
+
+  // submit data function
+  function submit(event) {
     event.preventDefault();
+    let input = document.querySelector("input");
+    if (input.value != "") addTask(input.value);
+    input.value = "";
+  }
 
-    // select task input
-    var taskInput = document.querySelector(".task");
-    // capture value of the task input
-    var taskValue = taskInput.value;
+  // add tasks
+  function addTask(task) {
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+    ul.appendChild(li);
+    document.querySelector(".tasksList").style.display = "block";
+  }
 
-    var task = document.createElement("li");
-    task.innerHTML = taskValue + " Remove";
-    var list = document.querySelector(".list");
+  // clear the LIST
+  function clearList(event) {
+    document.querySelector("ul").innerHTML = "";
+  }
 
-    list.appendChild(task);
-    list.addEventListener("click", function() {
-      task.remove();
-    });
-  });
+  // deleteTick
+  function deleteOrTick(event) {
+    var className = event.target.className;
+    if (className === "delete") deleteTask(event);
+    else {
+      tickTask(event);
+    }
+  }
+
+  // delete task
+  function deleteTask(event) {
+    let remove = event.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+  }
+
+  // tick a task
+  function tickTask(event) {
+    const task = event.target.nextSibling;
+    if (event.target.checked) {
+      task.style.textDecoration = "line-through";
+      task.style.color = "#ff0000";
+    } else {
+      task.style.textDecoration = "none";
+      task.style.color = "#000";
+    }
+  }
 });
